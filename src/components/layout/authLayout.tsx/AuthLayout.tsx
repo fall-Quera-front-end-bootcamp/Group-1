2ae622f29c2c1086f6c034db0af5321a8bc1a863
unknown <farzad.jafarpour@gmail.com>
2024-02-13 08:23:46 +0330

    css changed the background color

--- src/components/layout/authLayout.tsx/AuthLayout.tsx
+++ src/components/layout/authLayout.tsx/AuthLayout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function AuthLayout() {
   return (
     <div className="relative">
-      <div className="bg-gradient-to-br from-teal-500 to-blue-400 transform rotate-177 h-screen"></div>
+      <div className=" transform rotate-177 h-screen"></div>
       <span className="absolute inset-0 bg-white clip-polygon"></span>
     </div>
   );
