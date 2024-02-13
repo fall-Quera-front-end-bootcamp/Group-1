1acf7a5d9452ac684d2657fb638c8c317da6cbbd
unknown <farzad.jafarpour@gmail.com>
2024-02-13 08:07:12 +0330

    css login layout initialized

--- src/components/layout/authLayout.tsx/AuthLayout.tsx
+++ src/components/layout/authLayout.tsx/AuthLayout.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function AuthLayout() {
-  return <div>AuthLayout</div>;
+  return (
+    <div className="relative">
+      <div className="bg-gradient-to-br from-teal-500 to-blue-400 transform rotate-177 h-screen"></div>
+      <span className="absolute inset-0 bg-white clip-polygon"></span>
+    </div>
+  );
 }
 
 export default AuthLayout;
