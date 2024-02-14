633b5bce0243cf3186f00246a686e31384777f2a
Matin Chehrehsaz <Matin_Ch@Matin.local>
2024-02-15 01:48:47 +0330

    conflict is fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 // Import the necessary components from 'react-router-dom'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AuthLayout from "./components/layout/authLayout.tsx/AuthLayout";
-import TermsComponent from "./components/terms page";
+import LoginForm from "./components/login form/loginform";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AuthLayout />} />
-        {/* Add more routes as needed */}
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route path="login" index element={<LoginForm />} />
+            {/* Add more routes as needed */}
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
--- src/components/layout/authLayout.tsx/AuthLayout.tsx
+++ src/components/layout/authLayout.tsx/AuthLayout.tsx
@@ -1,13 +1,20 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../../navbar/navbar";
 
 function AuthLayout() {
   return (
-    <div className="relative z-0">
-      <div className=" transform rotate-177 h-screen"></div>
-      <span className="absolute inset-0 bg-white clip-polygon"></span>
+    <div>
+      <div className="nav">
+        <Navbar />
+      </div>
+      <div className="relative z-0">
+        <div className=" transform rotate-177 h-screen"></div>
+        <span className="absolute inset-0 bg-white clip-polygon"></span>
+      </div>
+      <div className="outlet">
+        <Outlet />
+      </div>
     </div>
   );
 }
-
 export default AuthLayout;
